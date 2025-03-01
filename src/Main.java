74cb9e71f94637e9927a4a58375324b9ec7858be
ovitudev <maximianodev@gmail.com>
2025-03-01 01:34:49 -0300

    Testing ...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import Controller.AdminController;
+import Model.AdminModel;
+import View.AdminView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        List<AdminModel> adminModelList = new ArrayList<>();
+
+
+        AdminController adminController = new AdminController(adminModelList);
+        AdminView adminView = new AdminView();
+
+        String choice;
+        do {
+            adminView.registrationAdmin(adminController, adminModelList);
+            choice = sc.nextLine();
+        } while (!choice.equals("0"));
+        adminView.showAdministrators(adminController);
     }
 }
