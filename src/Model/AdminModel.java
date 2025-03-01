bd49b9d5b200cf9997a23669df6b19e5f7740890
ovitudev <maximianodev@gmail.com>
2025-03-01 01:31:29 -0300

    Add Constructor and Method toString

--- src/Model/AdminModel.java
+++ src/Model/AdminModel.java
@@ -1,7 +1,12 @@
 package Model;
 
 public class AdminModel extends UserModel{
-    public AdminModel(String name, int accessLevel) {
-        super(name, accessLevel);
+    public AdminModel(String name, String cpf, String email, int id, int accessLevel) {
+        super(name, cpf, email, id, accessLevel);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString();
     }
 }
