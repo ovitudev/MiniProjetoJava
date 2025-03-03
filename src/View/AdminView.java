c7a7f483d5e46f6c6f0bc4ed6dd66af0e7dba124
ovitudev <maximianodev@gmail.com>
2025-03-02 22:35:39 -0300

    Added comments of functions

--- src/View/AdminView.java
+++ src/View/AdminView.java
@@ -7,34 +7,43 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AdminView {
-
+    // Instância do leitor para entrada de dados do usuário
     Scanner sc = new Scanner(System.in);
+
+    // Metódo responsável por realizar o cadastro de um usuário do tipo Administrador, pedindo o parâmetro da classe
+    // AdminController e da Listra do construtor dessa classe
     public void registrationAdmin(AdminController adminController, List<AdminModel> adminModelList){
         System.out.print("Enter a name: ");
         String name = sc.nextLine();
+        // Verificação responsável por analisar se há números na String digitada pelo usuário
         if(name.matches(".*\\d.*")){
             System.out.println("Error: Your name has invalid characters!");
-            return;
-        } else if (name.length() < 3) {
+            return; // Se tiver, será apresentada essa mensagem de que há characters inválidos e retornara para o início
+        } else if (name.length() < 3) { // Verificação de mínimo de characters aceitos pelo sistema
             System.out.println("Error: Your name has invalid number of characters!");
-            return;
+            return; // Caso seja menor, apresentar mensagem de quantidade characters pequeno e retorna para o início
         }
 
         System.out.print("Enter the cpf: ");
         String cpf = sc.nextLine();
+        // Verifica o tamanho mínimo e máximo do CPF, em casos de ser com ou sem máscara
+        // Futuramente, poderemos incrementar com uma verificação mais real, validando o cálculo do CPF real
         if(cpf.length() < 11 || cpf.length() > 14){
             System.out.println("Error: CPF is invalid!");
-            return;
-        } else if(cpf.matches(".*[a-zA-Z].*")){
+            return; // Caso não corresponda ao tamanho, irá retornar
+        }
+        // Verificação responsável por validar se há letras inclusas na String do CPF
+        else if(cpf.matches(".*[a-zA-Z].*")){
             System.out.println("Error: Your cpf has invalid characters!");
-            return;
+            return; // Caso tenha, irá retornar o erro e levar a pessoa para o início!
         }
 
         System.out.print("Enter a e-mail: ");
         String email = sc.nextLine();
+        // Verificação responsável por validar se o e-mail contém o @ (para domínios) e o mínimo do tamanho de um e-mail
         if(!email.contains("@") || email.length() < 6){
             System.out.println("Error: E-mail is invalid!");
-            return;
+            return; // Caso não, apresenta erro e retorna ao início
         }
 
         System.out.print("Enter the level access: ");
@@ -42,12 +51,20 @@ public class AdminView {
 
         sc.nextLine();
 
+        // Meio temporário de um id autoincrement, através do tamanho da lista
         int id = adminModelList.size() + 1;
 
+        // Passo as informações digitas pelo usuário para a lista de Admin
         adminController.registrationAdmin(name, cpf, email, id, accessLevel);
     }
 
+    // Metódo responsável por exibir a lista de Administradores cadastrados
+    // Como citei anteriormente, acredito que seria uma boa regra de negócio se os mesmos já fossem pré-cadastrados no
+    // sistema, pois dessa forma iriamos garantir veracidade nas informações e diminuir brechas para golpes!
+    // Mas de forma temporária, fizemos dessa maneira, como uma solução inicial da demanda!!!
     public void showAdministrators(AdminController adminController){
+        
+        //Verifica se a lista está vazia, se sim apresenta uma mensagem, se não, apresenta a lista de Administradores
         if (adminController.showAdministrators().isEmpty()){
             System.out.println("Error: no registration found!");
         } else {
